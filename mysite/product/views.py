e951d2fa05ef50a1322821cfe00d096b674e44c4
Christian <christian.w@hotmail.nl>
2019-07-11 19:41:27 +0200

    def index passes data from Product table to index.html, added comment for def index

--- mysite/product/views.py
+++ mysite/product/views.py
@@ -9,9 +9,13 @@ def new(request):
     return HttpResponse('add new product')
 
 
+"""
+gets 'product' which has all objects from 'Product' table stored,
+passes is to index.html for it be shown in the frontend
+"""
 def index(request):
     product = Product.objects.all()
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'products': product})
 
 
 def detail(request):
